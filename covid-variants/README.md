0a22d359b681e62bb98165d008db5145ae348c04
Daniel Nass <daniel.nass@corusent.com>
2021-12-16 15:48:36 -0500

    COVID variant data

---
+++ covid-variants/README.md
@@ -0,0 +1,14 @@
+# COVID-19 Canada Variant Data
+
+The data in this repository is published by [Global News](https://globalnews.ca) and comes from the weekly variant breakdown published by the [Public Health Agency of Canada](https://health-infobase.canada.ca/covid-19/epidemiological-summary-covid-19-cases.html#VOC). The data is updated once per week on Friday afternoon. [**Download the data in CSV format here.**](data/covid-19-variants-canada.csv)
+
+The data estimates variant prevalence as a percentage of overall cases, based on virus samples sequenced in laboratories across Canada. Because sequencing can take several weeks, the most recent estimates are delayed and may change as new samples are added.
+
+This repository works by downloading PHAC's detailed [variant estimate](https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-variants.csv) dataset and simplifying it to include only prevalence estimates for each designated variant of concern as well as a single estimate for all variants of interest. The prevalence of other variants is summarized in an "other" category. Those estimates are then joined with weekly [sample size](https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-variants-sampleSize.csv) data.
+
+An interactive visualization of the data can be seen in [this article](https://globalnews.ca/news/8404373/canada-travel-restrictions-south-africa-covid-19-variant/).
+
+![A chart of COVID-19 variant prevalence in Canada](charts/covid-19-variants-canada.png)
+
+## Changelog
+- 2021-12-TK: Initial publication of variant data.
---
+++ covid-variants/scripts/variant-data.R
@@ -0,0 +1,88 @@
+options(scipen = 999)
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(janitor)
+library(lubridate)
+library(scales)
+library(ggplot2)
+library(RColorBrewer)
+
+cutoff_week <- "2020-10-31"
+
+# PHAC variant prevalence data
+variant_data_original <- read_csv(
+  "https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-variants.csv"
+) %>% clean_names
+
+# PHAC variant sample size data
+variant_sample_size <- read_csv(
+  "https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-variants-sampleSize.csv"
+) %>% 
+  clean_names %>% 
+  rename(week = collection_date, n_samples = count_of_sample_number)
+
+data_max_week <- max(variant_data_original$collection_week)
+sample_max_week <- max(variant_sample_size$week)
+dataset_date_match <- data_max_week == sample_max_week
+
+if (dataset_date_match) {
+  variant_data <- variant_data_original %>% 
+
+    # Keep variants of concern as individual categories, 
+    # group variants of interest into a single category,
+    # and collapse everything else into "other"
+    mutate(variant_group = case_when(
+      variant_grouping == "VOC" ~ tolower(identifier),
+      variant_grouping == "VOI" ~ "voi",
+      TRUE ~ "other"
+    )) %>% 
+    group_by(week = collection_week, variant_group) %>% 
+    summarize(percent = sum(percent_ct_count_of_sample_number)) %>% 
+
+    # Weekly percentages don't always total exactly 100 — this step normalizes them
+    mutate(percent_normalized = round(percent / sum(percent), digits = 3)) %>% 
+    filter(week > cutoff_week)
+
+  last_updated <- format(now(), "%B %d, %Y at %I:%M %p %Z")
+
+  # Format and write the CSV data
+  variant_data_csv <- variant_data %>% 
+    select(-percent) %>% 
+    pivot_wider(
+      names_from = variant_group, 
+      values_from = percent_normalized, 
+      values_fill = 0
+    ) %>% 
+    ungroup %>% 
+    left_join(variant_sample_size, by = "week") %>% 
+    select(week, n_samples, everything()) 
+    # %>% 
+    # mutate(last_updated = ifelse(row_number() == 1, last_updated, ""))
+
+  variant_data_csv %>% write_csv("covid-variants/data/covid-19-variants-canada.csv")
+
+  # Save a chart
+  variant_chart <- variant_data %>% 
+    filter(variant_group != "other") %>% 
+    ggplot(aes(
+      x = week, 
+      y = percent_normalized, 
+      group = variant_group, 
+      fill = variant_group)
+    ) + 
+    geom_col() + 
+    scale_fill_manual(values = brewer.pal(7, "Purples")[2:7]) + 
+    theme_minimal() +
+    scale_y_continuous(labels = percent) +
+    labs(
+      title = "COVID-19 Variant Prevalence, Canada",
+      x = "", y = "", 
+      fill = "Variant",
+      caption = paste0("Source: Public Health Agency of Canada\nLast updated ", last_updated)
+    )
+
+  variant_chart %>% 
+    ggsave("covid-variants/charts/covid-19-variants-canada.png", ., device = "png", width = 8, height = 5)
+ }
